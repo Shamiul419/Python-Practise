9e6209b92c1dd9d54f2965bdbbcbbe261acfaa79
shamiul419 <samirazu30@gmail.com>
2020-09-09 02:03:43 +0600

    regular file added

--- random.py
+++ random.py
@@ -1,4 +1,7 @@
-import random
-numbers = random.randint(1, 10)
-for i in range(5):
-    print(f'{i} is {numbers}')
+import re
+messageValue = 'sas s fg gsafd dsa dgr gbob dad i gona love for ever plz call me 123-456-7890 '
+findValue = re.compile(r'\d\d\d\-\d\d\d\-\d\d\d\d')
+print(findValue.search(messageValue).group())
+#or
+#mo = findValue.search(messageValue)
+#print(mo.group())
